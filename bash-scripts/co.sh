b1aeefcd8802a52460880c61cac06219a4b8e0eb
Jason Cheung <CheungJ@users.noreply.github.com>
2017-04-19 11:29:29 +0100

    Create co.sh

---
+++ bash-scripts/co.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Usage: \$ co part_of_branch_name"
+	return 1
+fi
+
+BRANCH_COUNT=`git branch | grep -c $1`
+BRANCH=`git branch | grep $1`
+
+if [ $BRANCH_COUNT -eq 0 ]; then
+	echo "No branches found"
+	return 1
+elif [ $BRANCH_COUNT -gt 1 ]; then
+	printf  "More than one branch found:\n$BRANCH"
+	return 1
+fi
+
+git checkout $BRANCH
