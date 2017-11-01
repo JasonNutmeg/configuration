013590a7595fb3d948feab5d20a174a812a84b2e
Jason Cheung <CheungJ@users.noreply.github.com>
2017-11-01 17:27:54 +0000

    fix checkout of already-created branches

--- bash-scripts/co.sh
+++ bash-scripts/co.sh
@@ -7,24 +7,27 @@ if [ $# -eq 0 ]; then
 	return 1
 fi
 
-BRANCH_COUNT=`git branch -a | grep -c $1`
-BRANCH=`git branch -a | grep "$1" | xargs` # xargs trims the whitespace
+BRANCH_COUNT=`git branch | grep -c $1`
+BRANCH=`git branch | grep "$1" | xargs` # xargs trims the whitespace
 
 if [ $BRANCH_COUNT -eq 0 ]; then
-	echo "No branches found"
-	return 1
+	REMOTE_BRANCHES_COUNT=`git branch -a | grep -c "$1"`
+	REMOTE_BRANCH=`git branch -a | grep "$1" | xargs` # xargs trims the whitespace
+
+	if [ $REMOTE_BRANCHES_COUNT -eq 0 ]; then
+		echo "No branches found"
+		return 1
+	fi
+
+	if [[ $REMOTE_BRANCH == "remotes/origin/"* ]]; then # if starts with remote branch
+		BRANCH=`echo $REMOTE_BRANCH | cut -d'/' -f 3` # split string by '/' and use the 3rd substring
+	fi	
 elif [ $BRANCH_COUNT -gt 1 ]; then
 	printf  "More than one branch found:\n$BRANCH"
 	return 1
 elif [[ $BRANCH == \** ]]; then
 	printf  "Already on branch: $BRANCH"
 	return 1
-fi
-
-REMOTE_BRANCH="remotes/origin/"
-
-if [[ $BRANCH == $REMOTE_BRANCH* ]]; then # if starts with remote branch
-	BRANCH=`echo $BRANCH | cut -d'/' -f 3` # split string by '/' and use the 3rd substring
 fi
 
 git checkout $BRANCH
