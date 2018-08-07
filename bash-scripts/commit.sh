46b7bf75b18ef8a9bd626fbf1ed44fa54efe3fc8
Jason Cheung <CheungJ@users.noreply.github.com>
2018-08-07 10:48:37 +0100

    Handle branches like API-123_some-ticket-desc

--- bash-scripts/commit.sh
+++ bash-scripts/commit.sh
@@ -5,10 +5,11 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
+JIRA_SEPARATOR='_'
 USER_MESSAGE=$1
-JIRA_NUMBER=$(git rev-parse --abbrev-ref HEAD | cut -d '-' -f 1,2)
+JIRA_NUMBER=$(git rev-parse --abbrev-ref HEAD | cut -d $JIRA_SEPARATOR -f 1)
 
 COMMIT_MESSAGE="$JIRA_NUMBER: $USER_MESSAGE"
 
 echo $COMMIT_MESSAGE
-git commit -m $COMMIT_MESSAGE
+git commit -m "$COMMIT_MESSAGE"
